11e628739b73613b1b6b797fbec689674ef34da4
Husni <husnimubarakportofolio@gmail.com>
2025-04-27 09:17:51 +0700

    Update nav.php
    Menambahkan tautan ke page about

--- layout/nav.php
+++ layout/nav.php
@@ -2,6 +2,7 @@
         <nav class="flex justify-between items-center">
             <a href="index.php"><h1 class="text-xl font-bold">Gunting Batu Kertas RPG</h1></a>
             <ul class="flex gap-4">
+                <li><a href="about.php" class="hover:underline">Tentang Kami</a></li>
                 <li><a href="tautan.php" class="hover:underline">Tautan</a></li>
                 <li><a href="#" class="hover:underline">Kembali</a></li>
             </ul>
